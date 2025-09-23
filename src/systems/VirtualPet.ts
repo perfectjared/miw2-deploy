81b8eed77e7243d591642b22fb022682d59dc224
jared pettitt <jpettitt@ucsc.edu>
2025-09-22 21:57:54 -0700

    Deploy to GitHub Pages - Mon Sep 22 21:57:54 PDT 2025

--- src/systems/VirtualPet.ts
+++ src/systems/VirtualPet.ts
@@ -71,7 +71,7 @@ export class VirtualPet {
 	private petBaseColor: number = PET_CONFIG.petColor;
 
 	// Messaging system
-	private messageSprite?: Phaser.GameObjects.Sprite;
+	private messageSprite?: Phaser.GameObjects.Arc;
 	private messageText?: Phaser.GameObjects.Text;
 	private hasActiveMessage: boolean = false;
 	private messageThresholds: number[] = [3.0, 2.0, 1.5, 1.0, 0.5, 0.3, 0.2]; // 30%, 20%, 15%, 10%, 5%, 3%, 2%
